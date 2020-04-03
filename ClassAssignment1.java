01cc1b847e894a8efeae20d6f453f92fe10de302
rachit28d <13940739+rachit28d@users.noreply.github.com>
2020-04-03 21:15:56 +0530

    Add files via upload

--- ClassAssignment1.java
+++ ClassAssignment1.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.interactions.Actions;
 
 public class ClassAssignment1 {
 
-	private static final TimeUnit SECONDS = null;
+	
 
 	public static void main(String[] args) {
 		
@@ -17,20 +17,32 @@ public class ClassAssignment1 {
 	System.setProperty("webdriver.chrome.driver","./chromedriver.exe");
 	
 	ChromeDriver driver = new ChromeDriver ();
+	
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	//Login
 	driver.get("https://acme-test.uipath.com/account/login");
 	
-	//
+	//Enter Username
 	driver.findElementById("email").sendKeys("kumar.testleaf@gmail.com");
+	//Enter Password
 	driver.findElementById("password").sendKeys("leaf@12");
+	//Click on Login
 	driver.findElementById("buttonLogin").click();
 	
 	
-	driver.manage().timeouts().implicitlyWait(30, SECONDS);
+	//Mouse Over
 	WebElement ele = driver.findElementByXPath("//button[text()[normalize-space()='Vendors']]");
 	Actions builder = new Actions(driver);
 	builder.moveToElement(ele).perform();
 	driver.findElementByLinkText("Search for Vendor").click();
+	
+	driver.findElementById("vendorName").sendKeys("Blue Lagoon");
+	driver.findElementById("buttonSearch").click();
+	
+	WebElement findElementByXPath = driver.findElementByXPath("//td[text()='France']");
+	
+	driver.findElementByLinkText("Log Out").click();
+	driver.close();
 	
 		
 		
